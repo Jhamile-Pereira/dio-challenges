a7604d06f01cfa5ecce32dc713e9a53b81ba984e
Jhamile Pereira <jhamille.everlly23@gmail.com>
2023-11-09 09:57:20 -0300

    add solução do desafio Calculadora de Partidas Rankeadas

--- calculadora-partidas-rankeadas/calculadora-partidas-rankeadas.js
+++ calculadora-partidas-rankeadas/calculadora-partidas-rankeadas.js
@@ -1,7 +1,57 @@
+let vitorias = prompt("Quantidade de Vitórias:")
+let derrotas = prompt("Quantidade de Derrotas:")
+
 let saldo = calculatorRank(vitorias, derrotas)
+let rank = rankear(saldo)
 
+let texto1 = document.getElementById("texto1")
+let texto2 = document.getElementById("texto2")
+let saldoHtml = document.getElementById("saldo")
+let rankHtml = document.getElementById("rank")
 
+function rankear(saldo){
+    var rank = ""
+    if (saldo <= 10){
+        rank = "Ferro";
+    }
+    
+    else if (saldo > 10 && saldo < 21){
+        rank = "Bronze";
+    }
+    
+    else if (saldo > 20 && saldo < 51){
+        rank = "Prata";
+    }
+    
+    else if (saldo > 50 && saldo < 81){
+        rank = "Ouro";
+    }
+    
+    else if (saldo > 80 && saldo < 91){    
+        rank = "Diamante";
+    }
+    
+    else if (saldo > 90 && saldo < 101){    
+        rank = "Lendário";
+    }
 
-function calculatorRank(vitorias, derrotas){
-    return vitorias - derrotas
+    else if (saldo >= 101){
+        rank = "Imortal";
+    }
+    return rank
 }
+
+///Retorna 
+function calculatorRank(vitorias, derrotas){
+    let subs = vitorias - derrotas
+    return (subs)
+}
+
+///Modifica texto HTML para apresentar variáveis
+texto1.innerHTML = "O Herói tem saldo de  "
+saldoHtml.innerHTML = saldo
+texto2.innerHTML = " e está no nível "
+rankHtml.innerHTML = rank
+
+/// Mostra saldo e rank no console
+console.log(`O Herói tem de saldo de ${saldo} está no rank de ${rank}`)
